0de613dffd2438a2ddbeeee4569fb8e291577807
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 15:11:48 -0500

    Create model-teams-by-division.php

---
+++ model-teams-by-division.php
@@ -0,0 +1,16 @@
+<?php
+function selectTeamsByDivision($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, division_conference, divison_name, team_location, team_name FROM division d join team t on t.division_id=d.division_id WHERE team_id=?");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
