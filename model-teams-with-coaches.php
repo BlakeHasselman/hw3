d6b59ee51217069fe074b31373d5247bdca2e217
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 16:20:41 -0500

    Update model-teams-with-coaches.php

--- model-teams-with-coaches.php
+++ model-teams-with-coaches.php
@@ -12,4 +12,19 @@ function selectTeams() {
         throw $e;
     }
 }
+
+function selectCoachesByTeam($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT coach_id, coach_name, coach_position, team_location, team_name FROM coach c JOIN team t on t.team_id=c.team_id Where c.team_id=?;");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
