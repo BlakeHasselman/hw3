eecfc04737fdccbf8b3c5262acfbec158512a419
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 15:02:46 -0500

    Create teams-by-division.php

---
+++ teams-by-division.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-teams-by-division.php");
+
+$pageTitle = "Teams by Division";
+include "view-header.php";
+$coaches = selectTeamsByDivsion($_GET['id']);
+include "view-teams-by-divsion.php";
+include "view-footer.php";
+?>
