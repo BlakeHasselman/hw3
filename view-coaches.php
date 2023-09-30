5ee518e338645bc5b81b5cb1f6133cfd68dde844
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 16:03:16 -0500

    Update view-coaches.php

--- view-coaches.php
+++ view-coaches.php
@@ -6,6 +6,7 @@
       <th>ID</th>
       <th>Name</th>
       <th>Staff Position</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,12 @@ while ($coach = $coaches->fetch_assoc()) {
   <td><?php echo $coach['coach_id']; ?></td>
   <td><?php echo $coach['coach_name']; ?></td>
   <td><?php echo $coach['coach_position']; ?></td>
+  <td>
+    <form method="post" action="coaches-by-division.php">
+      <input type="hidden" name="cid" value="<?php echo $coach['coach_id']; ?>">
+        <button type="submit" class="btn btn-primary">Division</button>
+      </form>
+  </td>
 </tr>     
   <?php
   
