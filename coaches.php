4015924a54ff877848ab0a49a7bfb39572d3e604
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 13:26:34 -0500

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -4,7 +4,7 @@ require_once("model-coaches.php");
 
 $pageTitle = "Coaches";
 include "view-header.php";
-$teams = selectCoaches();
+$coaches = selectCoaches();
 include "view-coaches.php";
 include "view-footer.php";
 ?>
