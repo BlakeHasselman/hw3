0ecccb8d7aadd90d664c26c4f53af0870d855965
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 13:37:41 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -6,6 +6,7 @@
       <th>ID</th>
       <th>Name</th>
       <th>Location</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,7 @@ while ($team = $teams->fetch_assoc()) {
   <td><?php echo $team['team_id']; ?></td>
   <td><?php echo $team['team_name']; ?></td>
   <td><?php echo $team['team_location']; ?></td>
+  <td><a href="coaches-by-team.php?id=<?php echo $team['team_id']; ?>">Coaches</a></td>
 </tr>     
   <?php
   
