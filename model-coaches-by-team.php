f186aa76e9717028551535f255448e53d0692efe
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 14:00:46 -0500

    Create model-coaches-by-team.php

---
+++ model-coaches-by-team.php
@@ -0,0 +1,16 @@
+<?php
+function selectCoachesByTeam($tid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT coach_id, coach_name, coach_position, team_location, team_name FROM coach c JOIN team t on t.team_id=c.team_id Where c.team_id=?;");
+        $stmt->bind_param("i", $tid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
