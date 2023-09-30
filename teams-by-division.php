964e92bfadd633f649f9abc4cabad14145a626cd
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 15:17:16 -0500

    Update teams-by-division.php

--- teams-by-division.php
+++ teams-by-division.php
@@ -4,7 +4,7 @@ require_once("model-teams-by-division.php");
 
 $pageTitle = "Teams by Division";
 include "view-header.php";
-$divisions = selectTeamsByDivsion($_GET['id']);
+$division = selectTeamsByDivsion($_GET['id']);
 include "view-teams-by-divsion.php";
 include "view-footer.php";
 ?>
