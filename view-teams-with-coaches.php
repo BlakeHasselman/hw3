463b4c9bfdc4224e95cec058c404ae2d7c9d81f2
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-09-30 16:46:13 -0500

    Update view-teams-with-coaches.php

--- view-teams-with-coaches.php
+++ view-teams-with-coaches.php
@@ -7,14 +7,17 @@ while ($team = $teams->fetch_assoc()) {
     <div class="card-body">
       <h5 class="card-title"><?php echo $team['team_name']; ?></h5>
       <p class="card-text">
+        <ul class="list-group">
   <?php
     $coaches = selectCoachesByTeam($team['team_id']);
     while ($coach = $coaches->fetch_assoc()) {
 ?>
+      <li class="list-group-item"><?php echo $coach['coach_name']; ?> - <?php echo $coach['coach_position']; ?> - <?php echo $coach['team_location']; ?> - <?php echo $coach['team_name']; ?></li>
 <?php
     }
   ?>
-      </p>
+        </ul>
+        </p>
       <p class="card-text"><small class="text-body-secondary">:Location: <?php echo $team['team_location']; ?></small></p>
     </div>
   </div>
