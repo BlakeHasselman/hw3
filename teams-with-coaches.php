5391aaa29b9794dfaffcc31c8e32357972589fac
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 17:56:06 -0500

    Update teams-with-coaches.php

--- teams-with-coaches.php
+++ teams-with-coaches.php
@@ -14,7 +14,7 @@ if (isset($_POST['actionType'])) {
       }
         break;
     case "Edit":
-      if (updateCoachByTeam($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_id'])) {
+      if (updateCoachByTeam($_POST['cName'], $_POST['cPosition'], $_POST['team_id'])) {
         echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
