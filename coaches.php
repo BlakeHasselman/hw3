6be980287a0687e0098fa9c5c6538acded411757
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-24 23:49:26 -0500

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
+    case "Edit":
+      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
     case "Delete":
       if (deleteCoach($_POST['cid'])) {
         echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
