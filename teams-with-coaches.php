a811b98229064125e63e28c2e05286efdc00a055
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 01:33:50 -0500

    Update teams-with-coaches.php

--- teams-with-coaches.php
+++ teams-with-coaches.php
@@ -7,14 +7,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'])) {
+      if (insertCoach($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
         echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+      if (updateCoach($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
         echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
