b955aca3f4b768255c3173385beb1d1cb269efaa
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-24 23:10:44 -0500

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -4,6 +4,14 @@ require_once("model-coaches.php");
 
 $pageTitle = "Coaches";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'])
+        break;
+  }
+}
 $coaches = selectCoaches();
 include "view-coaches.php";
 include "view-footer.php";
