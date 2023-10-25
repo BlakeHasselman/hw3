6557df096218899d2d4626482042b0da3a0b9dc6
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 01:01:49 -0500

    Update view-teams-with-coaches.php

--- view-teams-with-coaches.php
+++ view-teams-with-coaches.php
@@ -1,4 +1,11 @@
+<div class="row">
+  <div class="col">
 <h1>Teams with Coaches</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-teams-with-coaches-newform.php";
+?>
 <div class="card-group">
 <?php
 while ($team = $teams->fetch_assoc()) {
@@ -8,11 +15,26 @@ while ($team = $teams->fetch_assoc()) {
       <h5 class="card-title"><?php echo $team['team_name']; ?></h5>
       <p class="card-text">
         <ul class="list-group">
+<?php
+include "view-teams-with-coaches-editform.php";
+?>
   <?php
     $coaches = selectCoachesByTeam($team['team_id']);
     while ($coach = $coaches->fetch_assoc()) {
 ?>
-      <li class="list-group-item"><?php echo $coach['coach_name']; ?> - <?php echo $coach['coach_position']; ?> - <?php echo $coach['team_location']; ?> - <?php echo $coach['team_name']; ?></li>
+      <li class="list-group-item"><?php echo $coach['coach_name']; ?> - <?php echo $coach['coach_position']; ?> - <?php echo $coach['team_location']; ?> - <?php echo $coach['team_name']; ?> <form method="post" action="">
+      <input type="hidden" name="cid" value="<?php echo $coach['coach_id']; ?>">
+      <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+          </svg>
+        </button>
+      </form> <form method="post" action="coaches-by-division.php">
+      <input type="hidden" name="cid" value="<?php echo $coach['coach_id']; ?>">
+        <button type="submit" class="btn btn-primary">Division</button>
+      </form> </li>
 <?php
     }
   ?>
