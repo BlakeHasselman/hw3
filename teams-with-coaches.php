b88c7afac56cbdee97011660139d7a9c4925eda1
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 01:50:32 -0500

    Update teams-with-coaches.php

--- teams-with-coaches.php
+++ teams-with-coaches.php
@@ -7,21 +7,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCoach($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
+      if (insertCoachByTeam($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
         echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updateCoach($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
+      if (updateCoachByTeam($_POST['cName'], $_POST['cPosition'], $_POST['team_location'], $_POST['team_name'])) {
         echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Delete":
-      if (deleteCoach($_POST['cid'])) {
+      if (deleteCoachByTeam($_POST['cid'])) {
         echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
