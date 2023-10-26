138ae86af44fee87acf0a87d9f4975d7dd4735da
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 19:05:00 -0500

    Update players.php

--- players.php
+++ players.php
@@ -4,6 +4,31 @@ require_once("model-players.php");
 
 $pageTitle = "Players";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlayer($_POST['tID'], $_POST['cName'], $_POST['cPosition'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
+    case "Edit":
+      if (updatePlayer($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
+    case "Delete":
+      if (deletePlayer($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;  }
+}
 $players = selectPlayers();
 include "view-players.php";
 include "view-footer.php";
