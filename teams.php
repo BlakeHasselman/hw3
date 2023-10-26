7d63781f45dc8ec400c1c36dc32804a67c36dd6f
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 19:44:51 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -4,6 +4,32 @@ require_once("model-teams.php");
 
 $pageTitle = "Teams";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
+    case "Edit":
+      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;
+    case "Delete":
+      if (deleteCoach($_POST['cid'])) {
+        echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+        break;  }
+}
+
 $teams = selectTeams();
 include "view-teams.php";
 include "view-footer.php";
