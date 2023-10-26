4e0eea62d0dabf4ea38a662f8b8a30536d2396ce
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 20:02:49 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'])) {
+      if (insertCoach($_POST['dID'], $_POST['tName'], $_POST['$tLocation'])) {
         echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+      if (updateCoach($_POST['dID'], $_POST['tName'], $_POST['$tLocation'], $_POST['tID'])) {
         echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Delete":
-      if (deleteCoach($_POST['cid'])) {
+      if (deleteCoach($_POST['tID'])) {
         echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
