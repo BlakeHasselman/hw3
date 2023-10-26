cf0c2e2b69f42dcad47358b0a7560a4e50f3164c
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 19:45:49 -0500

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -12,4 +12,46 @@ function selectTeams() {
         throw $e;
     }
 }
+
+function insertTeam($tID, $cName, $cPosition) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `coach` (`team_id`, `coach_name`, `coach_position`) VALUES (?, ?, ?)");
+        $stmt->bind_param("iss", $tID, $cName, $cPosition);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateTeam($tID, $cName, $cPosition, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `coach` set `team_id` = ?, `coach_name` = ?, `coach_position` = ? where coach_id = ?");
+        $stmt->bind_param("issi", $tID, $cName, $cPosition, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteTeam($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from coach where coach_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
