2caf0a2c2b169577b195365553e9ff9274a9e653
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-10-25 20:11:20 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -9,21 +9,21 @@ if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
       if (insertTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'])) {
-        echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
+        echo '<div class="alert alert-success" role="alert">Team Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
       if (updateTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'], $_POST['tID'])) {
-        echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
+        echo '<div class="alert alert-success" role="alert">Team Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Delete":
       if (deleteTeam($_POST['tID'])) {
-        echo '<div class="alert alert-success" role="alert">Coach Deleted!</div>';
+        echo '<div class="alert alert-success" role="alert">Team Deleted!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
