a5341cb10377303365e4954fe8fea69ed28caa25
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-13 14:02:28 -0600

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -2,7 +2,7 @@
 function selectPlayers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT player_id, player_name, player_position FROM player");
+        $stmt = $conn->prepare("SELECT player_id, player_name, player_position, team_id FROM player");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
