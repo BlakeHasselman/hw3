de058a22c27e9e60d402dfd6042fb193631c1057
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-15 00:51:22 -0600

    Update view-teams-chart.php

--- view-teams-chart.php
+++ view-teams-chart.php
@@ -1,4 +1,4 @@
-<h1>Coaches Chart</h1>
+<h1>Teams Chart</h1>
 <center>
     <div class="chart-container" style="height:80vh; width:100vw">
     <canvas id="myChart"></canvas>
@@ -7,31 +7,24 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
+const config = {
+  type: 'bar',
+  data: {
     datasets: [{
-        data: [
-<?php
-while ($coach = $coaches->fetch_assoc()) {
-  echo $coach['d.division_name'] . ", ";
-}
-?>
-
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-<?php
-$coaches = selectCoaches();
-while ($coach = $coaches->fetch_assoc()) {
-  echo "'" . $coach['team_name'] . "', ";
-}
-?>
-    ]
-},
-  });
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+        data: [10, 20, 30, 40, 50, 60, 70]
+    }]
+};,
+    
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
 </script>
