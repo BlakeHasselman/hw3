a873c3a5115f754e89befff7208af4596d0d6786
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-15 00:53:44 -0600

    Update view-teams-chart.php

--- view-teams-chart.php
+++ view-teams-chart.php
@@ -15,7 +15,12 @@ const config = {
         barThickness: 6,
         maxBarThickness: 8,
         minBarLength: 2,
-        data: [10, 20, 30, 40, 50, 60, 70]
+        data: 
+<?php
+while ($team = $teams->fetch_assoc()) {
+  echo $team['d.division_name'] . ", ";
+}
+?>
     }]
 };,
     
