c4d3bbe2b6f3e4dd19b6c98eaeae00bd34581abf
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-15 00:28:47 -0600

    Update view-coaches-chart.php

--- view-coaches-chart.php
+++ view-coaches-chart.php
@@ -3,7 +3,7 @@
   <canvas id="myChart"></canvas>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<div class="container">
+<div style="width=100px; height=100px;">
 <script>
   const ctx = document.getElementById('myChart');
 
