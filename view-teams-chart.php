a2e2768400a8258f5d9ba95a998c6e3f57edb26f
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-15 00:47:38 -0600

    Create view-teams-chart.php

---
+++ view-teams-chart.php
@@ -0,0 +1,37 @@
+<h1>Coaches Chart</h1>
+<center>
+    <div class="chart-container" style="height:80vh; width:100vw">
+    <canvas id="myChart"></canvas>
+    </div>
+</center>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+    datasets: [{
+        data: [
+<?php
+while ($coach = $coaches->fetch_assoc()) {
+  echo $coach['d.division_name'] . ", ";
+}
+?>
+
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+<?php
+$coaches = selectCoaches();
+while ($coach = $coaches->fetch_assoc()) {
+  echo "'" . $coach['team_name'] . "', ";
+}
+?>
+    ]
+},
+  });
+</script>
