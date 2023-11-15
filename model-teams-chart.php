a33d3d8e665ccc3e485764e3378b24c912e11562
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-11-15 00:46:33 -0600

    Create model-teams-chart.php

---
+++ model-teams-chart.php
@@ -0,0 +1,14 @@
+<?php
+function selectTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select team_name, d.divison_name from team t join division d on d.division_id=t.division_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
