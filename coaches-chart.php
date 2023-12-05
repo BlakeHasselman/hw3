2a029f9f399843f3d52195e51669fd2ebcccf44a
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-04 20:23:59 -0600

    Update coaches-chart.php

--- coaches-chart.php
+++ coaches-chart.php
@@ -2,10 +2,10 @@
 require_once("util-db.php");
 require_once("model-coaches-chart.php");
 
-$pageTitle = "Coaches Team Chart";
+$pageTitle = "Coaches Chart";
 include "view-header.php";
 $coaches = selectCoaches();
 include "view-coaches-chart.php";
 include "view-footer.php";
 ?>
-<p>This chart displays the different teams and how many coaches those teams have</p>
+
