a08fca221f8ab035a6bf5a7879e12d31e4f93fe5
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-04 20:43:12 -0600

    Update players.php

--- players.php
+++ players.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertPlayer($_POST['tID'], $_POST['pName'], $_POST['pPosition'])) {
+      if (insertPlayer($_POST['tID'], $_POST['pName'], $_POST['pPosition'], $_POST['pJersey'], $_POST['pHeight'], $_POST['pWeight'])) {
         echo '<div class="alert alert-success" role="alert">Player Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updatePlayer($_POST['tID'], $_POST['pName'], $_POST['pPosition'], $_POST['pid'])) {
+      if (updatePlayer($_POST['tID'], $_POST['pName'], $_POST['pPosition'], $_POST['pJersey'], $_POST['pHeight'], $_POST['pWeight'])) {
         echo '<div class="alert alert-success" role="alert">Player Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
