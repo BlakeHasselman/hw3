e9d4b3ea825c851695c1450efcbe5fba8f1baa74
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-04 20:21:25 -0600

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -11,7 +11,7 @@
     <div class="container">
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">NFL</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -33,7 +33,7 @@
           <a class="nav-link" href="players.php">Players</a>
         </li>    
         <li class="nav-item">
-          <a class="nav-link" href="coaches-chart.php">Coaches Chart</a>
+          <a class="nav-link" href="coaches-chart.php">Coaches Team Chart</a>
         </li>
       </ul>
     </div>
