8c2503f1b0c309f29f7c554d818272ee079d9343
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-04 20:52:02 -0600

    Update view-teams-with-coaches.php

--- view-teams-with-coaches.php
+++ view-teams-with-coaches.php
@@ -26,7 +26,7 @@ while ($team = $teams->fetch_assoc()) {
       <div class="col">
         <?php echo $coach['coach_name']; ?> - <?php echo $coach['coach_position']; ?> - <?php echo $team['team_location']; ?> - <?php echo $team['team_name']; ?>
       </div>
-      <div class="col-6">
+      <div class="col-auto">
 <?php
 include "view-teams-with-coaches-editform.php";
 ?>
