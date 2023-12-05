c4528bb7c803388cf6a92a7b36f576cf5b90209d
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-04 19:50:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,81 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-<h1>Homework 6</h1>
+<script>
+import React from 'react'
+
+import './page.css'
+
+const Page = (props) => {
+  return (
+    <div className="page-container">
+      <img
+        src="https://play.teleporthq.io/static/svg/default-img.svg"
+        className="page-image"
+      />
+      <div className="page-container1">
+        <h1 className="page-text">Project - NFL Database</h1>
+      </div>
+      <div className="page-container2">
+        <img
+          alt="image"
+          src="https://images.unsplash.com/photo-1512144253214-d94e86cd9189?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3w5MTMyMXwwfDF8c2VhcmNofDF8fG5mbHxlbnwwfHx8fDE3MDE3NDAxODV8MA&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1200"
+          className="page-image1"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Page
+</script>
+
+.page-container {
+  width: 100%;
+  display: flex;
+  overflow: auto;
+  min-height: 100vh;
+  align-items: center;
+  border-color: #0e44f3;
+  border-width: 5px;
+  flex-direction: column;
+  justify-content: center;
+}
+.page-image {
+  width: 483px;
+  height: 350px;
+  display: none;
+  z-index: 100;
+  box-shadow: 5px 5px 10px 0px #d4d4d4;
+  object-fit: cover;
+  border-color: var(--dl-color-gray-900);
+  border-width: 1px;
+  border-radius: var(--dl-radius-radius-radius4);
+}
+.page-container1 {
+  width: 100%;
+  height: 143px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.page-text {
+  font-weight: 700;
+  text-decoration: underline;
+}
+.page-container2 {
+  width: 100%;
+  height: 349px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+}
+.page-image1 {
+  width: 1110px;
+  height: 100%;
+  object-fit: cover;
+}
+
 <?php
 include "view-footer.php";
 ?>
