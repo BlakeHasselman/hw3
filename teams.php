dc946bbd24476d2fedc0c35a9eda957d082e46a5
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-06 16:01:58 -0600

    Update teams.php

--- teams.php
+++ teams.php
@@ -8,21 +8,21 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'], $_POST['tFounded'])) {
+      if (insertTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'], $_POST['tFounded', $_POST['ssWins'], $_POST['ssLosses'], $_POST['ssRank'], $_POST['ssPlayoff'])) {
         echo '<div class="alert alert-success" role="alert">Team Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updateTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'], $_POST['tFounded'], $_POST['tID'])) {
+      if (updateTeam($_POST['dID'], $_POST['tName'], $_POST['tLocation'], $_POST['tFounded'], $_POST['tID'], $_POST['ssWins'], $_POST['ssLosses'], $_POST['ssRank'], $_POST['ssPlayoff'], $_POST['ssID'])) {
         echo '<div class="alert alert-success" role="alert">Team Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Delete":
-      if (deleteTeam($_POST['tID'])) {
+      if (deleteTeam($_POST['tID'], $_POST['ssID'])) {
         echo '<div class="alert alert-success" role="alert">Team Deleted!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
