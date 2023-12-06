8a62c359ced6b9ce2b0121d5c8e746868e3d2480
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-06 14:47:58 -0600

    Update model-teams.php

--- model-teams.php
+++ model-teams.php
@@ -2,7 +2,7 @@
 function selectTeams() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT team_id, team_name, team_location, team_founded, division_id FROM `team`");
+        $stmt = $conn->prepare("SELECT t.team_id, stats_id, team_name, team_location, team_founded, ss.wins, ss.losses, ss.conference_rank, ss.playoff_status, division_id FROM `team` t join season_stats ss on ss.team_id=t.team_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -13,12 +13,16 @@ function selectTeams() {
     }
 }
 
-function insertTeam($dID, $tName, $tLocation, $tFounded) {
+function insertTeam($dID, $tName, $tLocation, $tFounded, $ssWins, $ssLosses, $ssRank, $ssPlayoff) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `team` (`division_id`, `team_name`, `team_location`, `team_founded`) VALUES (?, ?, ?)");
         $stmt->bind_param("issi", $dID, $tName, $tLocation, $tFounded);
         $success = $stmt->execute();
+        $teamId = $conn->insert_id;
+        $stmt2 = $conn->prepare("insert into `season_stats` (`wins`, `losses`, `conference_rank`, `playoff_status`) VALUES (?, ?, ?, ?)");
+        $stmt2->bind_param("iiis", $ssWins, $ssLosses, $ssRank, $ssPlayoff);
+        $stmt2->execute();
         $conn->close();
         return $success;
     } catch (Exception $e) {
@@ -27,12 +31,15 @@ function insertTeam($dID, $tName, $tLocation, $tFounded) {
     }
 }
 
-function updateTeam($dID, $tName, $tLocation, $tFounded, $tID) {
+function updateTeam($dID, $tName, $tLocation, $tFounded, $tID, $ssWins, $ssLosses, $ssRank, $ssPlayoff, $ssID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `team` set `division_id` = ?, `team_name` = ?, `team_location` = ?, `team_founded` = ? where team_id = ?");
         $stmt->bind_param("issii", $dID, $tName, $tLocation, $tFounded, $tID);
         $success = $stmt->execute();
+        $stmt2 = $conn->prepare("update `season_stats` set `wins` = ?, `losses` = ?, `conference_rank` = ?, `playoff_status` = ? where stats_id = ?");
+        $stmt2->bind_param("iiisi", $ssWins, $ssLosses, $ssRank, $ssPlayoff);
+        $stmt2->execute();
         $conn->close();
         return $success;
     } catch (Exception $e) {
@@ -41,11 +48,14 @@ function updateTeam($dID, $tName, $tLocation, $tFounded, $tID) {
     }
 }
 
-function deleteTeam($tID) {
+function deleteTeam($tID, $ssID) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("delete from team where team_id = ?");
         $stmt->bind_param("i", $tID);
+        $success = $stmt->execute();
+        $stmt = $conn->prepare("delete from season_stats where stats_id = ?");
+        $stmt->bind_param("i", $sID);
         $success = $stmt->execute();
         $conn->close();
         return $success;
