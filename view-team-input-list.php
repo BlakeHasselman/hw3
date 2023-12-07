431d98753ca73e8d23863a377396e63940efdfea
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-06 22:09:31 -0600

    Update view-team-input-list.php

--- view-team-input-list.php
+++ view-team-input-list.php
@@ -1,13 +1,9 @@
 <select class="form-select" id="tID" name="tID">
 <?php
 while ($teamItem = $teamList->fetch_assoc()) {
-  $selText = "";
-  if ($selectedTeam == $teamItem['team_id']) {
-     $selText = "selected";
-  }
-?>
-  <option value="<?php echo $teamItem['team_id']; ?>"<?=$selText?>><?php echo $teamItem['team_name']; ?></option>
-<?php
+  ?>
+  <option value="<?php echo $teamItem['team_id']; ?>"><?php echo $teamItem['team_name']; ?></option>
+  <?php
 }
 ?>
 </select>
