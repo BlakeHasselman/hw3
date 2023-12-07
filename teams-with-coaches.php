c0c6f9199ebc83def45f142e116a184eef98ca86
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-06 22:22:58 -0600

    Update teams-with-coaches.php

--- teams-with-coaches.php
+++ teams-with-coaches.php
@@ -7,7 +7,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCoachByTeam($_POST['tID'], $_POST['cID'] $_POST['cName'], $_POST['cPosition'])) {
+      if (insertCoachByTeam($_POST['tID'], $_POST['cID'], $_POST['cName'], $_POST['cPosition'])) {
         echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
