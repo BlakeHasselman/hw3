cfcb36b4674c07810be9376c850dc91bff887711
BlakeHasselman <123030379+BlakeHasselman@users.noreply.github.com>
2023-12-07 12:55:42 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -8,14 +8,14 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'])) {
+      if (insertCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cStart'], $_POST['cWins'], $_POST['cLosses'])) {
         echo '<div class="alert alert-success" role="alert">Coach Added!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
       }
         break;
     case "Edit":
-      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cid'])) {
+      if (updateCoach($_POST['tID'], $_POST['cName'], $_POST['cPosition'], $_POST['cStart'], $_POST['cWins'], $_POST['cLosses'], $_POST['cID'])) {
         echo '<div class="alert alert-success" role="alert">Coach Edited!</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error</div>';
